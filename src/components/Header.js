daa92d08fe1255b80f892a065f6aa8906c892dbd
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-09-17 01:48:05 -0400

    added className for sizing on images and alt tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,25 +24,41 @@ function Header() {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="container">
-              <img src="assets/images/image_db_cloud.jpg" />
+              <img
+                src="assets/images/image_db_cloud.jpg"
+                className="d-block w-100"
+                alt="RDBMS NOSQL Database Experience"
+              />
               <p></p>
             </div>
           </div>
           <div className="carousel-item">
             <div className="container">
-              <img src="assets/images/image_mern.png" />
+              <img
+                src="assets/images/image_mern.png"
+                className="d-block w-100"
+                alt="MERN MongoDB ExpressJS React Node Development"
+              />
               <p></p>
             </div>
           </div>
           <div className="carousel-item">
             <div className="container">
-              <img src="assets/images/image_debug.jpg" />
+              <img
+                src="assets/images/image_debug.jpg"
+                className="d-block w-100"
+                alt="Strong Troubleshooting Debugging Skills"
+              />
               <p></p>
             </div>
           </div>
           <div className="carousel-item">
             <div className="container">
-              <img src="assets/images/image_androidStudioJavaAndroid.png" />
+              <img
+                src="assets/images/image_androidStudioJavaAndroid.png"
+                className="d-block w-100"
+                alt="Android Studio Java Android Mobile Engineering"
+              />
               <p></p>
             </div>
           </div>
